e5507901fafcb19205bc313892697db97599e0cc
Adityaputt <gith6846@gmail.com>
2024-11-11 15:42:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.queryselectorall('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click' , function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrolllntoView({
+            behavior: 'smooth'
+        });
+    });
+})
